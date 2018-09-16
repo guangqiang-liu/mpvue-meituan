dca28242cdf72d9545c0cf054b51bd0b09eed6e2
刘光强 <liuguangqiang@qianbaocard.com>
2018-09-16 22:41:47 +0800

    修改配置文件

--- src/App.vue
+++ src/App.vue
@@ -80,4 +80,22 @@ page {
   background-color: #f9f9f9;
   min-height: 100%;
 }
+
+.container {
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+  padding: 200rpx 0;
+  box-sizing: border-box;
+}
+
+/* this rule will be remove */
+* {
+  transition: width 2s;
+  -moz-transition: width 2s;
+  -webkit-transition: width 2s;
+  -o-transition: width 2s;
+}
 </style>
