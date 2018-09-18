4621bf095a25a0381e3452c90a6b50f1524a117b
刘光强 <liuguangqiang@qianbaocard.com>
2018-09-18 17:47:09 +0800

    新增新增收货地址页面UI

---
+++ src/pages/addAddress/index.vue
@@ -0,0 +1,186 @@
+<template>
+  <div class="container">
+    <div class="name">
+      <span>联系人：</span>
+      <input placeholder="请填写收货人的姓名" placeholder-style="font-size: 28rpx" auto-focus/>
+    </div>
+    <div class="sex">
+      <div class="l"></div>
+      <div class="m">
+        <i class="icon qb-icon-down-arrow-s"></i>
+        <span>先生</span>
+      </div>
+      <div class="r">
+        <i class="icon qb-icon-down-arrow-s"></i>
+        <span>先生</span>
+      </div>
+    </div>
+    <div class="phone">
+      <span>手机号：</span>
+      <input placeholder="请填写收货人手机号码" placeholder-style="font-size: 28rpx" auto-focus/>
+    </div>
+    <div class="address">
+      <span class="l">收货地址：</span>
+      <div class="m">
+        <i class="icon qb-icon-down-arrow-s"></i>
+        <span>点击选择</span>
+      </div>
+      <div class="r">
+        <i class="icon qb-icon-down-arrow-s"></i>
+      </div>
+    </div>
+    <div class="house-num">
+      <span>门牌号：</span>
+      <input placeholder="详细地址，例：16号楼5楼301室" placeholder-style="font-size: 28rpx" auto-focus/>
+    </div>
+    <div class="submit">
+      <span>保存地址</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style lang="scss" scoped>
+.container {
+  background-color: white;
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  .name {
+    display: flex;
+    align-items: center;
+    margin-left: 30rpx;
+    padding-right: 30rpx;
+    height: 88rpx;
+    border-bottom: 2rpx solid $spLine-color;
+    span {
+      font-size: 28rpx;
+      color: $textBlack-color;
+      width: 160rpx;
+    }
+    input {
+      flex: 1;
+    }
+  }
+  .sex {
+    display: flex;
+    align-items: center;
+    margin-left: 30rpx;
+    padding-right: 30rpx;
+    height: 88rpx;
+    border-bottom: 2rpx solid $spLine-color;
+    .l {
+      width: 160rpx;
+    }
+    .m {
+      display: flex;
+      i {
+        font-size: 32rpx;
+        color: $theme-color;
+      }
+      span {
+        font-size: 32rpx;
+        color: $textBlack-color;
+        margin-left: 20rpx;
+      }
+    }
+    .r {
+      display: flex;
+      margin-left: 60rpx;
+      i {
+        font-size: 32rpx;
+        color: $theme-color;
+      }
+      span {
+        font-size: 32rpx;
+        color: $textBlack-color;
+        margin-left: 20rpx;
+      }
+    }
+  }
+  .phone {
+    display: flex;
+    align-items: center;
+    margin-left: 30rpx;
+    padding-right: 30rpx;
+    height: 88rpx;
+    border-bottom: 2rpx solid $spLine-color;
+    span {
+      font-size: 28rpx;
+      color: $textBlack-color;
+      width: 160rpx;
+    }
+    input {
+      flex: 1;
+    }
+  }
+  .address {
+    display: flex;
+    align-items: center;
+    margin-left: 30rpx;
+    padding-right: 30rpx;
+    height: 88rpx;
+    border-bottom: 2rpx solid $spLine-color;
+    .l {
+      font-size: 28rpx;
+      color: $textBlack-color;
+      width: 160rpx;
+    }
+    .m {
+      display: flex;
+      flex: 1;
+      i {
+        font-size: 28rpx;
+        color: $textBlack-color;
+      }
+      span {
+        font-size: 28rpx;
+        color:  $textGray-color;
+        margin-left: 20rpx;
+      }
+    }
+    .r {
+      i {
+        font-size: 28rpx;
+        color: $textBlack-color;
+      }
+    }
+  }
+  .house-num {
+    display: flex;
+    align-items: center;
+    margin-left: 30rpx;
+    padding-right: 30rpx;
+    height: 88rpx;
+    border-bottom: 2rpx solid $spLine-color;
+    span {
+      font-size: 28rpx;
+      color: $textBlack-color;
+      width: 160rpx;
+    }
+    input {
+      flex: 1;
+    }
+  }
+  .submit {
+    margin: 40rpx;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 76rpx;
+    background-color: $theme-color;
+    border-radius: 8rpx;
+    span {
+      font-size: 32rpx;
+      color: #472A00;
+    }
+  }
+}
+</style>
