cf286b520c7e0c3b723a6ec7981b83c83e1d63bc
刘光强 <liuguangqiang@qianbaocard.com>
2018-10-10 11:07:10 +0800

    优化工程代码

--- src/store/modules/submitOrder.js
+++ src/store/modules/submitOrder.js
@@ -1,16 +1,1 @@
 /** Created by guangqiang on 2018-09-28 23:17:03 */
-
-const state = {}
-
-const mutations = {}
-
-const actions = {}
-
-const getters = {}
-
-export default {
-  state,
-  mutations,
-  actions,
-  getters
-}
